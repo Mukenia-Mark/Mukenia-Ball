c97ffd7e3b2883b71d89921f2c9f8094db7233a4
Mukenia-Mark <mark.mukenia@gmail.com>
2021-06-02 14:29:25 +0300

    Add layout pages

---
+++ app/src/main/java/com/example/mukenia_ball/LeagueViewActivity.java
@@ -0,0 +1,6 @@
+package com.example.mukenia_ball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LeagueViewActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/mukenia_ball/TeamMenuActivity.java
@@ -0,0 +1,6 @@
+package com.example.mukenia_ball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeamMenuActivity extends AppCompatActivity {
+}

d6a1696e17ad6c4d13cfaa4eceff055180f5f627
Mukenia-Mark <mark.mukenia@gmail.com>
2021-06-03 00:20:25 +0300

    Add progressbar to team page

--- app/src/main/java/com/example/mukenia_ball/TeamArrayAdapter.java
+++ app/src/main/java/com/example/mukenia_ball/TeamArrayAdapter.java
@@ -1,4 +1,25 @@
 package com.example.mukenia_ball;
 
-public class TeamArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class TeamArrayAdapter extends ArrayAdapter {
+  private Context mContext;
+  private String[] mTeams;
+
+  public TeamArrayAdapter(Context mContext, int resource, String[] mTeams) {
+    super(mContext, resource);
+    this.mContext = mContext;
+    this.mTeams = mTeams;
+  }
+  @Override
+  public Object getItem(int position) {
+    String teams = mTeams[position];
+    return String.format("%s", teams);
+  }
+
+  @Override
+  public int getCount() {
+    return mTeams.length;
+  }
 }
--- app/src/main/java/com/example/mukenia_ball/TeamMenuActivity.java
+++ app/src/main/java/com/example/mukenia_ball/TeamMenuActivity.java
@@ -2,22 +2,35 @@ package com.example.mukenia_ball;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TeamMenuActivity extends AppCompatActivity {
-  @BindView (R.id.teamTitle) TextView mTeamTitle;
-  @BindView (R.id.teamList) ListView mTeamList;
-  private String[] teams = new String[] {"Manchester United", "Real Madrid", "Juventus"};
+  private static final String TAG = PlayerMenuActivity.class.getSimpleName();
+  @BindView(R.id.teamErrorTextView)
+  TextView mTeamErrorTextView;
+  @BindView(R.id.teamProgressBar)
+  ProgressBar mTeamProgressBar;
+  @BindView(R.id.teamList)
+  ListView mTeamList;
+  @BindView(R.id.teamTitle)
+  TextView mTeamTitle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +38,65 @@ public class TeamMenuActivity extends AppCompatActivity {
     setContentView(R.layout.activity_teammenu);
     ButterKnife.bind(this);
 
-    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, teams);
-    mTeamList.setAdapter(adapter);
     mTeamList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
-      public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        String team = ((TextView)view).getText().toString();
-        Toast.makeText(TeamMenuActivity.this, team,Toast.LENGTH_LONG).show();
+      public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        String team = ((TextView) view).getText().toString();
+        Toast.makeText(TeamMenuActivity.this, team, Toast.LENGTH_LONG).show();
       }
     });
     Intent intent = getIntent();
     String team = intent.getStringExtra("team");
     mTeamTitle.setText(team);
+
+
+    TeamApi client = TeamClient.getClient();
+    Call<MukeniaBallTeamResponses> call = client.getTeams(team);
+
+    call.enqueue(new Callback<MukeniaBallTeamResponses>() {
+      @Override
+      public void onResponse(Call<MukeniaBallTeamResponses> call, Response<MukeniaBallTeamResponses> response) {
+        hideProgressBar();
+        if (response.isSuccessful()) {
+          List<Team> teamList = response.body().getTeams();
+          String[] team = new String[teamList.size()];
+
+          for (int i = 0; i < team.length; i++) {
+            team[i] = teamList.get(i).getStrTeam();
+          }
+          ArrayAdapter adapter
+              = new PlayerArrayAdapter(TeamMenuActivity.this, android.R.layout.simple_list_item_1, team);
+          mTeamList.setAdapter(adapter);
+          showTeams();
+        } else {
+          showUnsuccessfulMessage();
+        }
+      }
+
+      @Override
+      public void onFailure(Call<MukeniaBallTeamResponses> call, Throwable t) {
+        Log.e(TAG,"onFailure",t);
+        hideProgressBar();
+        showFailureMessage();
+      }
+    });
+  }
+
+  private void showFailureMessage() {
+    mTeamErrorTextView.setText("Something went Wrong. Please Check your Internet connection and try again later");
+    mTeamErrorTextView.setVisibility(View.VISIBLE);
+  }
+
+  private void showUnsuccessfulMessage() {
+    mTeamErrorTextView.setText("Something went wrong. Please try again later");
+    mTeamErrorTextView.setVisibility(View.VISIBLE);
+  }
+
+  private void showTeams() {
+    mTeamList.setVisibility(View.VISIBLE);
+  }
+
+  private void hideProgressBar() {
+    mTeamProgressBar.setVisibility(View.GONE);
   }
 }
